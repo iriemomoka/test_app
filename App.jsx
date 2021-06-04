3efb454c77f47a6d9648cdabeba30a7696649f98
iriemomoka <83809826+iriemomoka@users.noreply.github.com>
2021-06-04 09:47:04 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetaileScree from './src/screens/MemoDetailScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetaileScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
---
+++ src/screens/MemoCreateScreen.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+   View, TextInput, StyleSheet, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBer from '../components/AppBer';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoCreateScreen() {
+  return (
+    <KeyboardAvoidingView behavior="height" style={styles.container}>
+      <AppBer />
+      <View style={styles.inputContainer}>
+        <TextInput value="" multiline style={styles.input} />
+      </View>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4fb',
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
