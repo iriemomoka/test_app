deb743d41c107550c128d5dff7a1ed79ff4464dd
iriemomoka <83809826+iriemomoka@users.noreply.github.com>
2021-06-08 16:12:58 +0900

    Minor fixes

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -62,17 +62,17 @@ MemoEditScreen.propTypes = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f4fb',
   },
   inputContainer: {
     flex: 1,
-    paddingHorizontal: 27,
-    paddingVertical: 32,
   },
   input: {
     flex: 1,
     textAlignVertical: 'top',
     fontSize: 16,
     lineHeight: 24,
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingHorizontal: 27,
   },
 });
